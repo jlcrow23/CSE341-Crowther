65d180d408d42fda00d7fe0346a3081990882501
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2023-04-26 11:35:23 -0600

    reinstalling essentials

--- Week 1-2/Lesson 1/server.js
+++ Week 1-2/Lesson 1/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const mongodb = require('.db/connect');
+const mongodb = require('./db/connect.js');
 const port = process.env.PORT || 3000;
 
-app.use('/', require('../routes'));
+app.use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
