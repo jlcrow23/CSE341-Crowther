4426159b42b7d5f0771900e0803b2a71051a3338
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2023-04-25 19:28:38 -0600

    fixing a hyphen

--- Lesson 1/Lesson 2/Personal/db/connect.js
+++ Lesson 1/Lesson 2/Personal/db/connect.js
@@ -2,29 +2,27 @@ const dotenv = require('dotenv');
 dotenv.config();
 const MongoClient = require('mongodb').MongoClient;
 
-let -db;
+let _db;
 
 const initDb = (callback) => {
-    if (_db) {
-        console.log('Db is already initialized!');
-        return callback(null, _db);
-    }
-    MongoClient.connect(process.env.MONGODB_URI)
-        .then((client) => {
-            _db = client;
-            callback(null, _db);
-        });
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
+  }
+  MongoClient.connect(process.env.MONGODB_URI).then((client) => {
+    _db = client;
+    callback(null, _db);
+  });
 };
 
 const getDb = () => {
-    if (!_db) {
-        throw Error('Db not initialized');
-    }
-    return _db;
+  if (!_db) {
+    throw Error('Db not initialized');
+  }
+  return _db;
 };
 
 module.exports = {
-    initDb,
-    getDb,
+  initDb,
+  getDb
 };
-
